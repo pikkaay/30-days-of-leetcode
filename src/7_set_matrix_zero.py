565e54f18343225519581f8bd60c94726d0ecaed
pikkaay <midhunpk.54@gmail.com>
2020-08-02 23:11:01 +0530

    set matrix zero

--- src/7_set_matrix_zero.py
+++ src/7_set_matrix_zero.py
@@ -62,8 +62,8 @@ matrix = [
 [1,1,1],
 [0,1,1],
 [1,1,1]]
-setZeroes(matrix)
-print(matrix)
+# setZeroes(matrix)
+# print(matrix)
 
 '''
 [
@@ -76,4 +76,74 @@ print(matrix)
 [1, 1, 1], 
 [0, 0, 0], 
 [1, 1, 1]]
+'''
+
+##################################################################################
+# O(1) Space 
+
+def setZeroes(matrix):
+    """
+    Do not return anything, modify matrix in-place instead.
+    """
+    # Handle first row and column
+    firstcol = firstrow = False
+    for i in range(len(matrix)):
+      if matrix[i][0] == 0:
+        firstcol = True
+        break
+
+    for i in range(len(matrix[0])):
+      if matrix[0][i] == 0:
+        firstrow = True
+        break
+
+    print(firstcol, firstrow)
+    for i in range(1, len(matrix)):
+        for j in range(1, len(matrix[0])):
+            if matrix[i][j] == 0:
+              matrix[i][0] = 0
+              matrix[0][j] = 0
+
+    print(matrix)
+    for i in range(1, len(matrix)):
+      if matrix[i][0] == 0:
+        for j in range(len(matrix[0])):
+          matrix[i][j] = 0
+
+    for i in range(1, len(matrix[0])):
+      if matrix[0][i] == 0:
+        for j in range(len(matrix)):
+          matrix[j][i] = 0
+
+    if firstcol:
+      for i in range(len(matrix)):
+        matrix[i][0] = 0
+
+    if firstrow:
+      for i in range(len(matrix[0])):
+        matrix[0][i] = 0
+
+
+matrix = [
+[1,1,1],
+[1,1,0],
+[1,1,1]
+]
+
+
+matrix = [
+[0,1,2,0],
+[3,4,5,2],
+[1,3,1,5]]
+setZeroes(matrix)
+print(matrix)
+
+
+'''
+[
+[0, 0, 0, 0], 
+[0, 4, 5, 0], 
+[0, 3, 1, 0]]
+
+
 '''
