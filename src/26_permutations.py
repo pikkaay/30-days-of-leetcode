80dff6f3ec9b3074451ae323ba938f26add1c119
pikkaay <midhunpk.54@gmail.com>
2020-08-12 22:16:31 +0530

    permutation

--- src/26_permutations.py
+++ src/26_permutations.py
@@ -1,6 +1,23 @@
 '''
 This is pure magic
 
+Permutations
+
+Given a collection of distinct integers, return all possible permutations.
+
+Example:
+
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+
 '''
 
 # not working solution
@@ -51,3 +68,33 @@ def permutation(nums):
 		return ls
 
 print(permutation([1,2, 3]))
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 0:
+            return []
+        if len(nums) == 1:
+            return [nums]
+        else:
+            ls = []
+            for i in range(len(nums)):
+                x = nums[i]
+                xs = nums[:i] + nums[i+1:]
+                for p in self.permute(xs):
+                    ls.append([x] + p)
+        return ls
+
+'''
+Run Code Status: Finished
+
+×
+Run Code Result:
+
+Your input
+
+[1,2,3]
+Your answer
+
+[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+'''
